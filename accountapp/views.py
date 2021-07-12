699dc2f313220370f0844d59737cffcb2ec048b0
yeonnex <yeonnex@gmail.com>
2021-07-12 17:30:24 +0900

    0712 커밋1

---
+++ accountapp/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from django.urls import reverse
+
+from accountapp.models import HelloWorld
+
+
+def hello_world(request):
+    if request.method == "POST":
+
+        temp = request.POST.get('input')
+
+        new_data = HelloWorld()
+        new_data.text = temp
+        new_data.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
+    else:
+        data_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'data_list': data_list})
